c192ee9c062eae1ae8401c5a3a6f1d2f73bd8a3a
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-05-10 20:56:44 +0100

    modified:   index.js 	new file:   routes/home.js 	modified:   views/index.ejs

---
+++ routes/home.js
@@ -0,0 +1,52 @@
+import express from "express"
+import axios from "axios"
+import bodyParser from "body-parser"
+import env from "dotenv"
+
+// const app = express()
+const homeRouter = express.Router();
+// const port = process.env.PORT || 3000
+const API_URL = "https://api.openweathermap.org/data/2.5/weather"
+env.config()
+
+const weather_apiKey = process.env.apikey;
+
+
+homeRouter.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+homeRouter.get('/', (req, res) => {
+  res.render('index.ejs')
+})
+
+homeRouter.post("/get-weather", async (req, res) => {
+    const cityName = req.body.city;
+    // const lon = req.body.lon;
+    // console.log(searchId);
+    // res.redirect('/')
+    // res.render('index.ejs', {
+    //     content: searchId
+    // })
+    const queryParams = {
+        params: {
+            q: cityName,
+            // lon: lon,
+            apikey: weather_apiKey
+        },
+    };
+    try {
+      const result = await axios.get(API_URL, queryParams);
+    //   console.log(result);
+      res.render("index.ejs", { content: result.data });
+    } catch (error) {
+        console.log(error.response.data);
+      res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+    }
+});
+
+export default homeRouter;
+
+// app.listen(port, () => {
+//     console.log(`port listening at ${port}`);
+// })
