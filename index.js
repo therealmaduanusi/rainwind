a48ce0670e50e2b03e5b4afaea03e38b5cf5803b
Victor Maduanusi <maduanusivictor2020@gmail.com>
2024-05-11 01:28:04 +0100

    modified:   index.js

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 import express from "express";
 import homeRouter from "./routes/home.js";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filename);
 
+// Set EJS as the view engine
+app.set('view engine', 'ejs');
+
+// Set the directory for views
+app.set("views", __dirname + "/views");
 
 // Mount the homeRouter to the '/home' path
 app.use("/", homeRouter);
